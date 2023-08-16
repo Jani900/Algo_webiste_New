13b9f228edba15f8142b833ddac31d53dca8f558
Jani900 <jani@godojo.fit>
2023-08-16 19:13:50 +1200

    new commit

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -5,121 +5,103 @@ import HeroContact from '../components/HeroContact'
 import { HiOutlinePhoneIncoming } from "react-icons/hi";
 import { HiOutlineMail } from "react-icons/hi";
 import ReCAPTCHA from "react-google-recaptcha";
-import { Link as ScrollLink } from 'react-scroll'
+import { Link as ScrollLink } from 'react-scroll';
+import emailjs from '@emailjs/browser'
+
 
 const Contact = () => {
-
-  
-        const { REACT_APP_RECAPTCHA_API_KEY_14082023 } = process.env;
-        const [loading, setLoading] = useState(false);
-        const [response, setResponse] = useState(null);
-        const [isCaptchaVerified, setIsCaptchaVerified] = useState(false);
-        const apiKey = "6Lfh4KQnAAAAAHKam4Or_IXDQGkvXxDI4L_jBGC3";
-        const testKey = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
-        console.log(
-          REACT_APP_RECAPTCHA_API_KEY_14082023 +
-            "test" +
-            apiKey +
-            "test2" +
-            testKey
-        ); 
-
-  
-
   //-----------------States-----------------//
-  const [focusedField, setFocusField] = useState(null)
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
-  })
+  const form = useRef();
+  const captchaRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+  const [isCaptchaVerified, setIsCaptchaVerified] = useState(false);
+  const apiKey = "6Lfh4KQnAAAAAHKam4Or_IXDQGkvXxDI4L_jBGC3";
+  const testKey = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
+  const serviceID = "service_sddgyff";
+  const templateID = "template_1hwr7zs";
+  const userID = "IJX4Qp4huwoVFCpsN";
+ 
 
+  const [focusedField, setFocusField] = useState(null);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+
+  const [name1, setName1] = useState("");
+  const [email1, setEmail1] = useState("");
+  const [message1, setMessage1] = useState("");
 
   //-----------------Functions-----------------//
 
 
-  const captchaRef = useRef(null);
-
-
-  const handleCaptchaVerify = (value) => { 
-    setIsCaptchaVerified(!!value)
-
+  const handleCaptchaVerify = (value) => {
+    setIsCaptchaVerified(!!value);
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-
-    const token = captchaRef.current.getValue();
+    e.preventDefault();
+    setLoading(true);
     captchaRef.current.reset();
-
-    
-
-    const {name, email, message} = formData
-    const subject = `New message from ${name}`
-    const body = `Name: ${name} \n Email: ${email} \n Message: ${message}`
-    const mailtoLink = `mailto:systems@algotech.co.nz?subject=${encodeURIComponent(
-      subject)}&body=${encodeURIComponent(body)}`;
 
     //check if the captcha is verified
 
     if (!isCaptchaVerified) {
-      alert('Please verify the captcha before submitting the form')
+      alert("Please verify the captcha before submitting the form");
       return;
     }
-if (isCaptchaVerified) {
-  window.location.href = mailtoLink;
-  alert('Thank you for your message. We will get back to you shortly')
-  setFormData({
-    name: '',
-    email: '',
-    message: ''
-  })
-  return;
-  }
-  else {
-console.error('There was an error sending the message');
-alert('There was an error sending the message')
-  }
-  }
+    if (isCaptchaVerified) {
+          emailjs.sendForm(serviceID, templateID, form.current, userID).then(
+            (result) => {
+               alert(
+                 "Thank you for your message. We will get back to you shortly"
+               );
 
+                setName1("");
+                setEmail1("");
+                setMessage1("");
 
+               setFormData({
+                 name: "",
+                 email: "",
+                 message: "",
+               });
+
+               console.log(result.text);
+               setLoading(false);
+               setIsCaptchaVerified(false);
+             
+            },
+            (error) => {
+              console.log(error.text);
+            }
+          );
+     
+      return;
+    } 
+    else 
+    {
+      console.error("There was an error sending the message");
+      alert("There was an error sending the message");
+    }
+  };
 
   const handlePhoneClick = () => {
-    alert('For phone enquiries call number: +64 22 620 6644')
-  }
+    alert("For phone enquiries call number: +64 22 620 6644");
+  };
 
   const handleEmailClick = () => {
-    alert('For email enquiries use: systems@algotech.co.nz')
-  }
+    alert("For email enquiries use: systems@algotech.co.nz");
+  };
 
-
-  const handleFocus = (e) => {
-    setFocusField(e.target.name)
-    setFocusField(e.target.email)
-    setFocusField(e.target.message)
-  }
-  const handleBlur = () => {
-    setFocusField(null)
-  }
+ 
 
   const isFieldFocused = (fieldName) => focusedField === fieldName;
 
 
-
-
-  const handleChange = (e) => {
-    const {name,email,message, value} = e.target
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-      [email]: value,
-      [message]: value
-    }))
-  }
-
-
-
-//--------------------------------------Render main--------------------------------------//
+  //--------------------------------------Render main--------------------------------------//
 
   return (
     <div>
@@ -161,14 +143,14 @@ alert('There was an error sending the message')
       <ScrollLink to="contact" smooth={true} duration={1000} />
       <div className="h-[650px]  bg-slate-50 w-full ">
         <form
-          action="https://formsubmit.co/jani@algotech"
-          method="POST"
+          key={formData}
+          ref={form} 
           className="flex flex-col items-center  w-full pt-20"
           onSubmit={handleSubmit}
         >
           <div
             className={`mb-4  ${
-              isFieldFocused("name") ? "border-[#81cdba]" : ""
+              isFieldFocused("name") ? "border-[#81cdba]" : "#000"
             }`}
           >
             <label
@@ -179,13 +161,13 @@ alert('There was an error sending the message')
             </label>
             <input
               type="text"
-              name="name"
+              name="from_name"
+              value={name1}
               id="name"
-              onChange={handleChange}
-              onFocus={() => handleFocus("name")}
               className=" px-4 py-2 border border-[#606161] rounded-md focus:outline-none focus:ring-2 focus:ring-[#81cdba] focus:border-transparent
               w-[300px] lg:w-[650px] sm:w-[400px]"
               required
+              onChange={(e) => {setName1(e.target.value)}}
             />
           </div>
           <div className="mb-4">
@@ -197,13 +179,13 @@ alert('There was an error sending the message')
             </label>
             <input
               type="email"
-              name="email"
+              name="from_email"
               id="email"
-              onChange={handleChange}
-              onFocus={() => handleFocus("email")}
               className=" px-4 py-2 border border-[#606161] rounded-md focus:outline-none focus:ring-2 focus:ring-[#81cdba] focus:border-transparent
              w-[300px] lg:w-[650px] sm:w-[400px]"
               required
+              onChange={(e) => {setEmail1(e.target.value)}}
+              value={email1}
             />
           </div>
           <div className="mb-4">
@@ -217,32 +199,25 @@ alert('There was an error sending the message')
               type="text"
               name="message"
               id="message"
-              onChange={handleChange}
-              onFocus={() => handleFocus("message")}
               rows="5"
               className=" px-4 py-2 border border-[#606161] rounded-md focus:outline-none focus:ring-2 focus:ring-[#81cdba] focus:border-transparent
               w-[300px] lg:w-[650px] sm:w-[400px]"
               required
+              onChange={(e) => {setMessage1(e.target.value)}}
+              value={message1}
             />
           </div>
-          <input type="hidden" name="_captcha" value="true" />
-          <input
-            type="hidden"
-            name="_autoresponse"
-            value="Thank you for your message. We will get back to you shortly"
-            className='mb-4'
+        
+          <ReCAPTCHA
+            className="mb-4"
+            ref={captchaRef}
+            sitekey={apiKey}
+            onChange={handleCaptchaVerify}
           />
-          <input type="hidden" name="_template" value="table" />
-            <ReCAPTCHA
-            className='mb-4'
-              ref={captchaRef}
-              sitekey={testKey}
-              onChange={handleCaptchaVerify}
-             
-              />
 
           <button
             type="submit"
+            value="Send"
             className="px-28 py-2 text-[#81cdba] bg-[#606161] rounded-md hover:bg-[#81cdba] hover:text-[#606161] focus:outline-none focus:bg-[#606161] focus:text-[#81cdba] lg:px-64 lg:py-3 w-[300px] lg:w-[650px] sm:w-[400px]"
           >
             Submit
